0089d2fe68ec76363deb9e0f539803ff018a8b19
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-05-19 13:31:14 +0300

    use ref to count render component

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
+    const ref = useRef(0);
+    const [defState, setDefState] = useState(false);
+    const toggleDefState = () => {
+        setDefState((ps) => !ps);
+    };
+
+    useEffect(() => ref.current++);
+
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            <p>render count:{ref.current}</p>
+            <button className="btn btn-primary" onClick={toggleDefState}>
+                Toggle {defState}
+            </button>
         </CardWrapper>
     );
 };
